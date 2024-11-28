054762bd5181150162168c8276a95b9524f626bc
Stefano Mammola <68010284+StefanoMammola@users.noreply.github.com>
2024-11-28 08:37:06 +0100

    updated legend of figure 1

--- Script/Main_analysis.R
+++ Script/Main_analysis.R
@@ -14,7 +14,7 @@
 # Loading R packages ------------------------------------------------------
 
 if(!require("pacman")) {install.packages("pacman")}
-pacman::p_load("dplyr","ggalluvial","ggplot2","tibble","tidyr")
+pacman::p_load("cowplot","dplyr","ggalluvial","ggplot2","tibble","tidyr")
 
 # Script settings -----------------------------------------------------------
 theme_set(theme_minimal())
@@ -76,11 +76,104 @@ table(db[db$Subt > 0,]$Section)/table(db$Section)*100
 
 # Plotting ecosystem services vs stakeholders ---------------------------------------------
 
+# Custom colors
 my.colors.system <- c("Terrestrial" = "#A0522D", 
                       "Freshwater" = "#5DADE2", 
                       "Marine" = "#154360")
 
-db |>
+### Generating figure 1 ###
+(figure1 <- db |> dplyr::select(Section, 
+                                Terrestrial, 
+                                Freshwater,
+                                Marine, 
+                                Assessed_Ter, 
+                                Assessed_Fre, 
+                                Assessed_Mar) |>
+    {\(.) {replace(.,is.na(.),0)}}()  |> #replace NA
+    tidyr::pivot_longer(cols = c(Terrestrial, Freshwater, Marine),
+                        names_to = "System",
+                        values_to = "Presence") |> 
+    dplyr::group_by(Section,System) |>
+    dplyr::summarise(TotalPresence = sum(Presence, na.rm = TRUE),
+                     .groups = 'drop') |>
+    dplyr::mutate(TotalTested = #adding total number of tested services
+                    db |> dplyr::select(Section, 
+                                        Assessed_Ter, 
+                                        Assessed_Fre, 
+                                        Assessed_Mar) |>
+                    {\(.) {replace(.,is.na(.),0)}}()  |> #replace NA
+                    tidyr::pivot_longer(cols = c(Assessed_Ter, Assessed_Fre, Assessed_Mar),
+                                        names_to = "Tested",
+                                        values_to = "Assessed") |> 
+                    dplyr::group_by(Section,Tested) |>
+                    dplyr::summarise(TotalTested = sum(Assessed, na.rm = TRUE),
+                                     .groups = 'drop') |>
+                    dplyr::pull(TotalTested),
+                  TotalCount = #adding total number of services
+                    table(db$Section) |> 
+                    data.frame() |> 
+                    dplyr::pull(Freq) |> 
+                    rep(each = 3)
+    ) |>
+    mutate(
+      PercentTotal = (TotalPresence / TotalCount) * 100,
+      PercentTested = (TotalTested / TotalPresence) * 100,
+      Label = paste0(round(PercentTotal, 1), "% (",round(PercentTested, 1),"%)"),
+      System = factor(System, levels = c("Terrestrial", "Freshwater", "Marine")), 
+      Section = factor(Section, levels = c("Provisioning", "Regulation & Maintenance", "Cultural"))) |>
+    #plotting
+    ggplot(aes(x = System)) +
+    geom_bar(aes(y = TotalCount), stat = "identity", fill = "white",color = "grey10", show.legend = FALSE) +   # Outer bar for TotalCount
+    geom_bar(aes(y = TotalPresence, fill = System), stat = "identity", alpha = 0.6, color = NA, show.legend = FALSE) +   # Inner bar for TotalCount
+    geom_bar(aes(y = TotalTested, fill = System), stat = "identity", color = NA, show.legend = FALSE) +   # Inner bar for TotalCount
+    geom_text(aes(y = TotalPresence + 2, label = Label, color = System), 
+              size = 3, show.legend = FALSE) +
+    facet_wrap(~ Section) +
+    scale_fill_manual(values = my.colors.system) +
+    scale_color_manual(values = my.colors.system) +
+    labs(title = NULL, x = NULL, y = "Number of ecosystem services") + 
+    theme(
+      strip.text = element_text(size = 12, face = "bold"),
+      panel.spacing = unit(1, "lines"),
+      axis.text.x = element_text(angle = 45, hjust = .8)
+    ))
+
+(figure1_legend <- data.frame( # Create a dummy dataset for the legend
+  Category = factor(c("TotalCount", "TotalPresence", "TotalTested"),
+                    levels = c("TotalCount", "TotalPresence", "TotalTested")),
+  Value = c(100, 80, 40),
+  Label = rev(c("All\nservices", "Untested\nsubterranean\nservices", "Tested\nsubterranean\nservices"))
+) |> 
+    dplyr::mutate(CumValue = cumsum(Value) - (Value / 2)) |> 
+    ggplot(aes(x = 1, y = Value, fill = Category)) +
+    geom_bar(stat = "identity", position = "identity", width = 0.5, alpha = 0.5,color = "grey5") +
+    geom_text(aes(y = c(20,60,90), label = Label),  # Use calculated midpoints
+              size = 3, color = "grey5") +
+    geom_text(aes(y = 105, label = "% Subterranean (% Tested)"), color = "grey10",
+              size = 3, show.legend = FALSE) +
+    scale_fill_manual(values = c(
+      "TotalCount" = "white",
+      "TotalPresence" = "grey70",
+      "TotalTested" = "grey20"
+    )) +
+    theme_void() +  # Remove unnecessary elements
+    labs(title = " ", x = " ") + # white space
+    theme(
+      plot.title = element_text(size = 50, face = "bold", hjust = 0.5),
+      legend.position = "none",
+      plot.margin = margin(10, 50, 50, 10),
+      axis.title.x = element_text(size = 1200, angle = 90, hjust = .2))
+)
+
+(figure1 <- cowplot::plot_grid(
+  figure1,                      
+  figure1_legend,                  
+  ncol = 2,                       
+  rel_widths = c(5, 1)            
+)) ; rm(figure1_legend) # Combine the main plot and the legend
+
+### Generating figure 2 ###
+(figure2 <- db |>
   tidyr::separate_rows(Stakeholder, sep = ";") |> # Splits rows by ";"
   tidyr::pivot_longer(cols = c(Terrestrial, Freshwater, Marine),
                         names_to = "System",
@@ -103,120 +196,33 @@ db |>
                    #axis.text.y = element_blank(),      # Remove y-axis text
                    #axis.ticks.y = element_blank(),     # Remove y-axis ticks
                    axis.text.x = element_text(size = 12) # Enlarge x-axis tick text
-                 )
+                 ))
 
-####
-(figure2 <- db |> dplyr::select(Section, 
-                    Terrestrial, 
-                    Freshwater,
-                    Marine, 
-                    Assessed_Ter, 
-                    Assessed_Fre, 
-                    Assessed_Mar) |>
-  {\(.) {replace(.,is.na(.),0)}}()  |> #replace NA
-  tidyr::pivot_longer(cols = c(Terrestrial, Freshwater, Marine),
-                      names_to = "System",
-                      values_to = "Presence") |> 
-  dplyr::group_by(Section,System) |>
-  dplyr::summarise(TotalPresence = sum(Presence, na.rm = TRUE),
-                   .groups = 'drop') |>
-  dplyr::mutate(TotalTested = #adding total number of tested services
-                  db |> dplyr::select(Section, 
-                                      Assessed_Ter, 
-                                      Assessed_Fre, 
-                                      Assessed_Mar) |>
-                  {\(.) {replace(.,is.na(.),0)}}()  |> #replace NA
-                  tidyr::pivot_longer(cols = c(Assessed_Ter, Assessed_Fre, Assessed_Mar),
-                                      names_to = "Tested",
-                                      values_to = "Assessed") |> 
-                  dplyr::group_by(Section,Tested) |>
-                  dplyr::summarise(TotalTested = sum(Assessed, na.rm = TRUE),
-                                   .groups = 'drop') |>
-                  dplyr::pull(TotalTested),
-                TotalCount = #adding total number of services
-                  table(db$Section) |> 
-                  data.frame() |> 
-                  dplyr::pull(Freq) |> 
-                  rep(each = 3)
-  ) |>
-  mutate(
-    PercentTotal = (TotalPresence / TotalCount) * 100,
-    PercentTested = (TotalTested / TotalPresence) * 100,
-    Label = paste0(round(PercentTotal, 1), "% (",round(PercentTested, 1),"%)"),
-    System = factor(System, levels = c("Terrestrial", "Freshwater", "Marine")), 
-    Section = factor(Section, levels = c("Provisioning", "Regulation & Maintenance", "Cultural"))) |>
-  #plotting
-  ggplot(aes(x = System)) +
-  geom_bar(aes(y = TotalCount), stat = "identity", fill = "white",color = "grey10", show.legend = FALSE) +   # Outer bar for TotalCount
-  geom_bar(aes(y = TotalPresence, fill = System), stat = "identity", alpha = 0.6, color = NA, show.legend = FALSE) +   # Inner bar for TotalCount
-  geom_bar(aes(y = TotalTested, fill = System), stat = "identity", color = NA, show.legend = FALSE) +   # Inner bar for TotalCount
-  geom_text(aes(y = TotalPresence + 2, label = Label, color = System), 
-            size = 3, show.legend = FALSE) +
-  facet_wrap(~ Section) +
-  scale_fill_manual(values = my.colors.system) +
-  scale_color_manual(values = my.colors.system) +
-  labs(title = NULL, x = NULL, y = "Number of ecosystem services") + 
-  theme(
-    strip.text = element_text(size = 12, face = "bold"),
-    panel.spacing = unit(1, "lines"),
-    axis.text.x = element_text(angle = 45, hjust = .8)
-  ))
+### Saving the figures ###
 
-## Custom legend
+# Figure 1
+pdf(file = "Figures/Figure_1.pdf", width = 15, height = 8)
+figure1
+dev.off()
 
-library(patchwork) # For combining plots
+# Figure 2
+pdf(file = "Figures/Figure_2.pdf", width = 12, height = 5)
+figure2
+dev.off()
 
-# Create a dummy dataset for the legend
-legend_data <- data.frame(
-  Category = factor(c("TotalCount", "TotalPresence", "TotalTested"),
-                    levels = c("TotalCount", "TotalPresence", "TotalTested")),
-  Value = c(100, 60, 40),
-  Label = rev(c("All\nservices", "Subterranean\nservices", "Tested\nsubterranean\nservices"))
-)
+# ggpubr::ggarrange(M1.2.forest_plot,
+#                   ggpubr::ggarrange(M1.venn, M2.venn, 
+#                                     ncol = 2, hjust = -5, vjust = 4,
+#                                     labels = c("B", "C")),
+#                   common.legend = FALSE,
+#                   hjust = -5,
+#                   #align = "h",
+#                   labels = c("A", ""),
+#                   ncol=1, nrow=2) 
 
-# Calculate cumulative values for positioning
-legend_data <- legend_data |> 
-  dplyr::mutate(CumValue = cumsum(Value) - (Value / 2))  # Midpoint of each segment
+# Figure 2 ----------------------------------------------------------------
 
-# Create the custom legend
-(custom_legend <- ggplot(legend_data, aes(x = 1, y = Value, fill = Category)) +
-  geom_bar(stat = "identity", position = "identity", width = 0.5, alpha = 0.5,color = "black") +
-  geom_text(aes(y = c(20,50,80), label = Label),  # Use calculated midpoints
-            size = 3, color = c("white","white","black")) +
-  # geom_text(aes(y = 65, label = "Subterranean %\n(Tested subterranean %)"), color = "darkblue",
-  #            size = 2, show.legend = FALSE) +
-  scale_fill_manual(values = c(
-    "TotalCount" = "white",
-    "TotalPresence" = "blue",
-    "TotalTested" = "darkblue"
-  )) +
-  theme_void() +  # Remove unnecessary elements
-  labs(title = NULL, x = "                                             ") +
-  theme(
-    plot.title = element_text(size = 10, face = "bold", hjust = 0.5),
-    legend.position = "none",
-    plot.margin = margin(10, 50, 50, 10),
-    axis.title.x = element_text(angle = 90, hjust = .8))
-  )
-
-
-
-
-library(cowplot)
-
-# Combine the main plot and the legend
-final_plot <- cowplot::plot_grid(
-  figure2,                      # Your existing main plot
-  custom_legend,                  # The custom legend
-  ncol = 2,                       # Arrange side by side
-  rel_widths = c(5, 1)            # Adjust the width ratio (main plot wider)
-)
-
-final_plot
-
-
-
-# 
+ 
 # db |> dplyr::select(Section, 
 #                     Terrestrial, 
 #                     Freshwater,
